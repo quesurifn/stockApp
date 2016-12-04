489392cdb9da1e6f80eb3f9c4419cb582cebbf95
Kyle <kyle.c.r.fahey@gmail.com>
2016-12-04 09:30:32 -0600

    Add forms

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var socketio = require('socket.io');
 var mongoose = require('mongoose');
+var yahooFinance = require('yahoo-finance');
 require('./models/model.js');
 
 
